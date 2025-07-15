a695ffb6536e6ee3f2c62aea9a5215d195995185
ArcticHonour <122731187+ArcticHonour@users.noreply.github.com>
2025-07-15 20:36:37 +0100

    Update req.ps1

--- req.ps1
+++ req.ps1
@@ -1,5 +1,22 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; `
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; `
+# Check the current execution policy
+$currentPolicy = Get-ExecutionPolicy
+
+if ($currentPolicy -eq 'Restricted') {
+    Write-Output "Execution policy is Restricted. Setting to AllSigned..."
+    try {
+        Set-ExecutionPolicy -ExecutionPolicy AllSigned -Scope CurrentUser -Force
+    }
+    catch {
+        Write-Warning "Failed to set policy to AllSigned, setting to Bypass for process scope."
+        Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+    }
+} else {
+    Write-Output "Execution policy is $currentPolicy. No need to change."
+}
+
+# Run the Chocolatey install script with Bypass for the process scope
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install -y python3
